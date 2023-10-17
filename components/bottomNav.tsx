2aff70579c4b09aeef2ac30c87c2309fd8cb9a7f
Judah High <judah.high@protonmail.com>
2023-10-17 10:55:41 -0600

    Adjusted styling for light and dark mode on the bottom nav bar.

--- components/bottomNav.tsx
+++ components/bottomNav.tsx
@@ -2,9 +2,9 @@ import * as elements from "typed-html";
 
 export function BottomNav() {
   return (
-    <div class="btm-nav bg-base-200 border-t border-gray-700">
+    <div class="btm-nav dark:bg-base-200 bg-gray-800 border-t border-gray-700">
       <a
-        class="hover:text-primary-content hover:bg-base-300 border-r border-gray-700"
+        class="text-white hover:text-primary-content hover:bg-gray-950 border-r border-gray-700"
         href="https://github.com/judahhigh"
       >
         <img src="../public/github.png"></img>
@@ -12,7 +12,7 @@ export function BottomNav() {
       </a>
       <a
         href="https://www.linkedin.com/in/judah-high"
-        class="hover:text-primary-content hover:bg-base-300"
+        class="text-white hover:text-primary-content hover:bg-gray-950"
       >
         <img src="../public/linkedin.png"></img>
         Linkedin
